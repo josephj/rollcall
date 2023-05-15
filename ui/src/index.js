aac020d200adb5d7978f1c49e3d739c145bbb1a4
Joseph Chiang <joseph.chiang@ignitionapp.com>
2023-05-15 15:57:18 +1000

    Fix: route

--- ui/src/index.js
+++ ui/src/index.js
@@ -24,6 +24,10 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
+    path: "/:org",
+    element: <Gatherings />,
+  },
+  {
     path: "/:org/gatherings",
     element: <Gatherings />,
   },
