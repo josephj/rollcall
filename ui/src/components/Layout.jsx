d47e1d9f21f891229bfcb41f8eb00e8a6a8bdc16
Joseph Chiang <josephj6802@gmail.com>
2023-07-29 21:30:12 +1000

    Add a alert so people knows it's a testing site

--- ui/src/components/Layout.jsx
+++ ui/src/components/Layout.jsx
@@ -1,9 +1,10 @@
-import { Flex } from "native-base";
+import { Alert, Flex, HStack, Text, InfoIcon } from "native-base";
 
 import { Header } from "./Header";
 import { LoadingSpinner } from "./LoadingSpinner";
 
 export const Layout = ({ headerContent, children, isLoading }) => {
+  const isDevelopment = process.env.REACT_APP_DATA_SET !== "production";
   return (
     <Flex flexDirection="column" height="100%">
       {headerContent && <Header>{headerContent}</Header>}
@@ -17,6 +18,19 @@ export const Layout = ({ headerContent, children, isLoading }) => {
         paddingY="5"
         position="relative"
       >
+        {isDevelopment && (
+          <Alert
+            variant="left-accent"
+            colorScheme="info"
+            width="300px"
+            marginBottom={5}
+          >
+            <HStack space={2} alignItems="center">
+              <InfoIcon color="blue.400" />
+              <Text flexGrow={1}>You are using the testing site.</Text>
+            </HStack>
+          </Alert>
+        )}
         {children}
         {isLoading && <LoadingSpinner isOverlay />}
       </Flex>
