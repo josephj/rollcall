4e8d74828a2425be5418a5d4ca43ba47c23660d8
Joseph Chiang <josephj6802@gmail.com>
2023-08-13 06:17:00 +1000

    Introduce sanity-codegen

--- api/sanity.config.ts
+++ api/sanity.config.ts
@@ -2,17 +2,31 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {RobotIcon, RocketIcon} from '@sanity/icons'
 
-export default defineConfig({
-  name: 'default',
-  title: 'efcsydney-rollcall',
-
-  projectId: '5wmwst53',
-  dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'production-workspace',
+    title: '點名系統（正式環境）',
+    projectId: '5wmwst53',
+    dataset: 'production',
+    basePath: '/production',
+    icon: RocketIcon,
+    plugins: [deskTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'staging-workspace',
+    title: '點名系統（測試環境）',
+    projectId: '5wmwst53',
+    dataset: 'development',
+    basePath: '/staging',
+    icon: RobotIcon,
+    plugins: [deskTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
--- ui/src/apolloClient.ts
+++ ui/src/apolloClient.ts
@@ -1,6 +1,7 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const dataset = process.env.REACT_APP_DATA_SET || "development";
 export const apolloClient = new ApolloClient({
-  uri: 'https://5wmwst53.api.sanity.io/v1/graphql/production/default',
+  uri: `https://5wmwst53.api.sanity.io/v1/graphql/${dataset}/default`,
   cache: new InMemoryCache(),
 });
